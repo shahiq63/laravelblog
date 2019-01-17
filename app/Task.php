8928dd898ddd195de5b856a835dcaefaaa84921a
Syed Shahiq <syed.shahiq@square63.com>
2019-01-17 12:12:58 +0500

    Toggle Status Function added in Task model

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+  public $timestamps = false;
+
+  public function toggleStatus()
+  {
+    $this->status = !$this->status;
+    return $this;
+  }
 }
