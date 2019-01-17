11248661e37b9233d287c2ff09ea8557d30b8095
Syed Shahiq <syed.shahiq@square63.com>
2019-01-17 12:34:52 +0500

    CSRF Field added

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -6,8 +6,11 @@
   
   <h2 style="margin:5px">My To Do List</h2>
   <form method="POST" action="{{ route('create') }}">
-    <input type="text" id="myInput" placeholder="Title...">
+    <input type="text" name ="task" id="myInput" placeholder="Title...">
     <input type="submit" value="Add" class="addBtn">
+
+    {{ csrf_field() }}
+  
   </form>
 
 </div>
