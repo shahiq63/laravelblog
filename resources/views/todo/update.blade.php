a2b14c0b6320e8130a21498a0174235afd69d35c
Syed Shahiq <syed.shahiq@square63.com>
2019-01-28 13:06:29 +0500

    Update view updated

--- resources/views/todo/update.blade.php
+++ resources/views/todo/update.blade.php
@@ -7,12 +7,23 @@
   <h2 style="margin:5px">Update Task</h2>
   <form method="POST" enctype="multipart/form-data" action="{{ route('updated',['id'=>$task->id]) }}">
     <div class="form-group">
-      <input type="text" class="form-control" name ="task" id="myInput" placeholder="Title..." value="{{$task->content}}">
+      <input type="text" class="form-control" name ="task" id="myInput" placeholder="Title..." value="{{$task->title}}">
     </div>
+    
+    <div class="form-group">
+      <textarea class="form-control" name="description" placeholder="Task Description">{{$task->description}}
+      </textarea>
+    </div>
+    
+    <select class="form-group form-control" name="username">
+      <option class="form-control"value="{{$task->username}}" selected="selected">{{$task->username}}</option>
+      @foreach($users as $user)
+        <option class="form-control"value="{{$user->name}}">{{$user->name}}</option>
+      @endforeach
+    </select>
 
     <div class="form-group">
-    <input type="submit" value="Update" class="btn btn-primary">
-      
+      <input type="submit" value="Update" class="btn btn-primary">
     </div>
 
     {{ csrf_field() }}
