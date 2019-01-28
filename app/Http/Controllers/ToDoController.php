a1a24c042b33b1e97160588dbb1bae2741488a3b
Syed Shahiq <syed.shahiq@square63.com>
2019-01-28 13:10:02 +0500

    Contoller updated

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -7,15 +7,21 @@ use Illuminate\Http\Request;
 use App\Task;
 use Auth;
 use DB;
+use App\User;
 class ToDoController extends Controller
 {
   public function index()
   {
-    // $tasks = Task::all();
     if(isset(Auth::user()->name)){
       $name = Auth::user()->name;
-      $tasks = DB::table('tasks')->where('username', $name)->get();
-      return view('todo.index',['tasks'=>$tasks]);
+      if($name == 'admin'){
+        $users = User::all();
+        $tasks = Task::all();
+        return view('admin.add_task',['users'=>$users,'tasks'=>$tasks]);
+      }else {
+        $tasks = DB::table('tasks')->where('username', $name)->get();
+        return view('todo.index',['tasks'=>$tasks]);
+      }
     }else {
       return view('todo.index');
     }
@@ -24,17 +30,17 @@ class ToDoController extends Controller
   public function create(Request $request)
   {
      $this->validate($request,[
-      'task' => 'required|unique:tasks,content',
-      'photo'=> 'required'
+      'task' => 'required|unique:tasks,title',
      ]);
      $file = $request->file('photo');
      $filename = 'profile-photo-' . time() . '.' . $file->getClientOriginalExtension();
      $path = $file->storeAs('photos', $filename);
      if ($request->input('task') && $request->file('photo') && isset(Auth::user()->name)) {
       $task = new Task;
-      $task->content = $request->input('task');
+      $task->title = $request->input('task');
+      $task->description = $request->input('description');
       $task->Image_path = $path;
-      $task->username = Auth::user()->name;
+      $task->username = $request->get('username');
       $task->save();
       return redirect()->back();
     }else {
@@ -45,14 +51,17 @@ class ToDoController extends Controller
   public function update($id)
   {
     $task = Task::find($id);
-     return view('todo.update',['task'=>$task]);
+    $users = User::all();
+    return view('todo.update',['task'=>$task,'users'=>$users]);
   }
 
   public function updated($id , Request $request)
   {
     $task = Task::find($id);
     if($request->input('task')) {
-      $task->content = $request->input('task');
+      $task->title = $request->input('task');
+      $task->description = $request->input('description');
+      $task->username = $request->get('username');
       $task->save();
     }
     return redirect('/');
