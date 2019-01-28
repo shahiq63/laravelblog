2d6223ae2887be305b0bca76cb616bc9c89156bc
Syed Shahiq <syed.shahiq@square63.com>
2019-01-28 13:05:05 +0500

    Index Page updated

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -2,60 +2,26 @@
 
 @section('content')
 
-<div id="myDIV" class="container">
-  
-  <h2 style="margin:5px">My To Do List</h2>
-  <form method="POST" enctype="multipart/form-data" action="{{ route('create') }}">
-    <div class="form-group">
-      <input type="text" name ="task" id="myInput" placeholder="Title" class="form-control" required>
+ @if(isset($tasks))
+  <div id="accordion" class="container">
+  @foreach($tasks as $task)
+ <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapse{{$task->id}}" aria-expanded="true" aria-controls="collapseOne">
+          {{$task->title}}
+        </button>
+      </h5>
     </div>
-    <div class="form-group">
-      <input type="file" name="photo" class="form-control-file" required>
+
+    <div id="collapse{{$task->id}}" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        {{$task->description}}
+      </div>
     </div>
-    <div class="form-group">
-    <button type="Submit" class="btn btn-primary">Submit</button>
   </div>
-    {{ csrf_field() }}
-  
-  </form>
+  @endforeach
 
-</div>
-
- @if(count($errors)>0)
-
-    <ul class="container">
-      @foreach($errors->all() as $error)
-
-      <li class="alert alert-danger"> {{$error}}</li>
-      @endforeach
-    </ul>
-
- @endif
-
-<ul id="myUL" class="container">
-  @if(isset($tasks))
-  <?php foreach($tasks as $task):?>
-
-    <?php if($task->status == 0):?>
-    
-    <li class="list-group-item list_item" onclick="location.href='{{ route('update',['id'=>$task->id]) }}'">
-      <?php echo $task->content;?>
-      <a href="{{ route('delete',['id'=>$task->id]) }}" class="close">x</a>
-    </li>
-
-    <?php endif;?>
-
-    <?php if($task->status == 1):?>
-    
-    <li  class="list-group-item list_item" onclick="location.href='{{ route('update',['id'=>$task->id]) }}'">
-     <?php echo $task->content; ?> 
-     <a href="{{ route('delete',['id'=>$task->id]) }}"class="close">x</a>
-   </li>
-
-    <?php endif;?>
-
-  <?php endforeach ?>
+    </div>
   @endif
-  </ul>
-
 @endsection
